1216a02b6dd8af82a44d32e09ec0fc2872374bae
Nikhil <nk9599094257@gmail.com>
2023-08-30 00:36:21 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -28,6 +28,15 @@ app.use(cors());
 //   }
 //   next();
 // });
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://bloging-website-client.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
 app.use(morgan("dev"));
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
