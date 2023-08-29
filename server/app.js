16845af673edc5c3735177852a10378a707bf952
Nikhil <nk9599094257@gmail.com>
2023-08-30 00:59:01 +0530

    revert

--- server/app.js
+++ server/app.js
@@ -26,9 +26,7 @@ app.use(morgan("dev"));
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',()=>{
-  res.json('Hello')
-})
+
 
 app.use("/", Routes);
 
